23681cb868f43c11ee886b0526ba4c6d798def23
Oualid Aamarri <oualidrbm@gmail.com>
2025-02-05 23:30:36 +0100

    Initial Commit

---
+++ MyFitnessPal/MyFitnessPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFitnessPalApp.swift
+//  MyFitnessPal
+//
+//  Created by Oualid AAMARRI on 05/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyFitnessPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
